011858a91fb6ba2fce31c427b770b12bd1b2956f
Jesus HM <jesusjejeje0406@gmail.com>
2025-10-25 15:21:42 -0600

    Feat: dynamic delivery option selection using dayjs library

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -1,14 +1,22 @@
-import { cart } from "../../data/cart.js";
+import { cart, updateDeliveryOption } from "../../data/cart.js";
 import { products } from "../../data/products.js";
+import { deliveryOptions } from "../../data/deliverOptions.js";
+import dayjs from "https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js";
 
 export function renderOrderSummary(){
   let orderSummaryHTML = '';
   cart.forEach(cartItem => {
     const matchedProduct = products.find(productItem => productItem.id === cartItem.productId);
 
+    const { deliveryId } = cartItem;
+    const today = dayjs();
+    const dateDays = deliveryOptions.find(option=> option.id === deliveryId).days;
+    let deliveryDate = today.add(dateDays, 'days');
+    deliveryDate = deliveryDate.format('dddd, MMMM D');
+
     orderSummaryHTML += `
       <div class="cart-item-container">
-        <div class="delivery-date">Delivery date: Tuesday, June 21</div>
+        <div class="delivery-date">Delivery date: ${deliveryDate}</div>
 
         <div class="cart-item-details-grid">
           <img class="product-image" src="${matchedProduct.image}">
@@ -32,36 +40,49 @@ export function renderOrderSummary(){
           <div class="delivery-options">
             <div class="delivery-options-title">Choose a delivery option:</div>
 
-            <div class="delivery-option">
-              <input type="radio" checked class="delivery-option-input" name="delivery-option-${matchedProduct.id}">
+            ${createOptionsHTML(matchedProduct.id)}
 
-              <div>
-                <div class="delivery-option-date">Tuesday, June 21</div>
-                <div class="delivery-option-price">FREE Shipping</div>
-              </div>
-            </div>
-
-            <div class="delivery-option">
-              <input type="radio" class="delivery-option-input" name="delivery-option-${matchedProduct.id}">
-
-              <div>
-                <div class="delivery-option-date">Wednesday, June 15</div>
-                <div class="delivery-option-price">$4.99 - Shipping</div>
-              </div>
-            </div>
-
-            <div class="delivery-option">
-              <input type="radio" class="delivery-option-input" name="delivery-option-${matchedProduct.id}">
-
-              <div>
-                <div class="delivery-option-date">Monday, June 13</div>
-                <div class="delivery-option-price">$9.99 - Shipping</div>
-              </div>
-            </div>
           </div>
         </div>
       </div>
     `;
   });
   document.querySelector('.order-summary').innerHTML = orderSummaryHTML;
-}
+
+  function createOptionsHTML(productId){
+    let optionsHTML = '';
+    
+    deliveryOptions.forEach(option=>{
+      const matchedProduct = cart.find(cartItem => cartItem.productId === productId);
+
+      if(matchedProduct){
+        const today = dayjs();
+        let deliveryDate = today.add(option.days, 'days');
+        deliveryDate = deliveryDate.format('dddd, MMMM D');
+        const matchedOption = matchedProduct.deliveryId;
+        const priceString = option.priceCents === 0 ? 'FREE' : `$${option.priceCents / 100} -`;
+
+        optionsHTML += `
+        <div class="delivery-option" data-product-id="${productId}" data-delivery-id="${option.id}">
+          <input type="radio" ${matchedOption === option.id ? 'checked' : ''} class="delivery-option-input" name="delivery-option-${productId}">
+
+          <div>
+            <div class="delivery-option-date">${deliveryDate}</div>
+            <div class="delivery-option-price">${priceString} Shipping</div>
+          </div>
+        </div>
+        `;
+      }
+    });
+
+    return optionsHTML;
+  }
+
+  document.querySelectorAll('.delivery-option').forEach(option=>{
+    option.addEventListener('click',()=>{
+      const { productId,deliveryId } = option.dataset;
+      updateDeliveryOption(productId, deliveryId);
+      renderOrderSummary();
+    });
+  });
+};
