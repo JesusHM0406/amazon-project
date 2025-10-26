5bee0cfc0720aca4e3c231fd250685b6578589f8
Jesus HM <jesus.h.mora0406@gmail.com>
2025-10-26 16:25:29 -0600

    feat: add payment summary section HTML based on variables

--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -21,4 +21,37 @@ function renderPaymentSummary(){
   const total = formatCurency(totalCents);
 
   const cartQuantity = calculateCartQuantity();
+
+  const paymentSummaryHTML = `
+    <div class="payment-summary-title">Order Summary</div>
+
+    <div class="payment-summary-row">
+      <div>Items (${cartQuantity}):</div>
+      <div class="payment-summary-money">$${formatCurency(priceCents)}</div>
+    </div>
+
+    <div class="payment-summary-row">
+      <div>Shipping &amp; handling:</div>
+      <div class="payment-summary-money">$${formatCurency(shippingPriceCents)}</div>
+    </div>
+
+    <div class="payment-summary-row subtotal-row">
+      <div>Total before tax:</div>
+      <div class="payment-summary-money">$${formatCurency(beforeTaxCents)}</div>
+    </div>
+
+    <div class="payment-summary-row">
+      <div>Estimated tax (10%):</div>
+      <div class="payment-summary-money">$${formatCurency(taxCents)}</div>
+    </div>
+
+    <div class="payment-summary-row total-row">
+      <div>Order total:</div>
+      <div class="payment-summary-money">$${total}</div>
+    </div>
+
+    <button class="place-order-button button-primary">
+      Place your order
+    </button>
+  `;
 }
