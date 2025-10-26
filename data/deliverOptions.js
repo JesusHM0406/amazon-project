e529f947966d970dbc823e51067064257ac41dcc
Jesus HM <jesus.h.mora0406@gmail.com>
2025-10-26 16:54:27 -0600

    feat(deliveryOptions.js): add dayjs library

--- data/deliverOptions.js
+++ data/deliverOptions.js
@@ -1,3 +1,5 @@
+import dayjs from "https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js";
+
 export const deliveryOptions = [
   {id: '1', priceCents: 0, days: 7},
   {id: '2', priceCents: 499, days: 3},
