0a9a43ca30e2fb3a6cfeccb9e7d40f54902fb6b9
Jesus HM <jesus.h.mora0406@gmail.com>
2025-10-28 17:35:18 -0600

    test: add full suite for loadFromStorage function

--- test/data/cartTest.js
+++ test/data/cartTest.js
@@ -1,1 +1,41 @@
-import { cart, calculateCartQuantity } from "../../data/cart.js";
+import { cart, calculateCartQuantity, loadFromStorage } from "../../data/cart.js";
+
+describe('loadFromStorage',()=>{
+  it('returns the cart if localStorage returns a valid JSON',()=>{
+    spyOn(localStorage, 'getItem').and.returnValue(JSON.stringify([
+      { productId: '123', quantity: 2, deliveryId: '1' },
+      { productId: '321', quantity: 1, deliveryId: '2' }
+    ]));
+
+    loadFromStorage();
+
+    expect(cart).toEqual([
+      { productId: '123', quantity: 2, deliveryId: '1' },
+      { productId: '321', quantity: 1, deliveryId: '2' }
+    ]);
+  });
+
+  it('returns an empty array if the cart is empty or not initialized',()=>{
+    spyOn(localStorage, 'getItem').and.returnValue(null);
+
+    loadFromStorage();
+
+    expect(cart).toEqual([]);
+  });
+
+  it('returns an empty array if localStorage not returns an array',()=>{
+    spyOn(localStorage, 'getItem').and.returnValue(JSON.stringify(5));
+
+    loadFromStorage();
+
+    expect(cart).toEqual([]);
+  });
+
+  it('returns an empty array if localStorage returns a non-valid JSON',()=>{
+    spyOn(localStorage, 'getItem').and.returnValue(JSON.stringify(undefined));
+
+    loadFromStorage();
+
+    expect(cart).toEqual([]);
+  });
+})
