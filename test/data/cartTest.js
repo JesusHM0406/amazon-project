27169cc19ccd7bbb400fa2ed7422831a719b83eb
Jesus HM <jesus.h.mora0406@gmail.com>
2025-10-28 17:45:57 -0600

    test: add full suite for calculateCartQuantity function

--- test/data/cartTest.js
+++ test/data/cartTest.js
@@ -38,4 +38,52 @@ describe('loadFromStorage',()=>{
 
     expect(cart).toEqual([]);
   });
-})
+});
+
+  describe('calculateCartQuantity',()=>{
+  it('returns the total of items in the cart',()=>{
+    spyOn(localStorage, 'getItem').and.returnValue(JSON.stringify([
+      { productId: '123', quantity: 2, deliveryId: '1' },
+      { productId: '321', quantity: 1, deliveryId: '2' }
+    ]));
+
+    loadFromStorage();
+
+    expect(calculateCartQuantity()).toEqual(3);
+  });
+
+  it('works with empty cart',()=>{
+    spyOn(localStorage, 'getItem').and.returnValue(JSON.stringify([]));
+
+    loadFromStorage();
+
+    expect(calculateCartQuantity()).toEqual(0);
+  });
+
+  it('works when all products have quantity 0', ()=>{
+    spyOn(localStorage, 'getItem').and.returnValue(JSON.stringify([
+      { productId: '123', quantity: 0, deliveryId: '1' },
+      { productId: '321', quantity: 0, deliveryId: '2' }
+    ]));
+
+    loadFromStorage();
+
+    expect(calculateCartQuantity()).toEqual(0);
+  });
+
+  it('returns 0 when localStorage returns null or undefined',()=>{
+    spyOn(localStorage, 'getItem').and.returnValue(undefined);
+
+    loadFromStorage();
+
+    expect(calculateCartQuantity()).toEqual(0);
+  });
+
+  it('returns 0 when localStorage not returns an array',()=>{
+    spyOn(localStorage, 'getItem').and.returnValue(5);
+
+    loadFromStorage();
+
+    expect(calculateCartQuantity()).toEqual(0);
+  });
+});
