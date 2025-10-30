e25fffe8b777e4684510008c13cf45db8bd5e449
Jesus HM <jesus.h.mora0406@gmail.com>
2025-10-30 12:23:27 -0600

    refactor(orderSummary.js): improve the modify quantity event to prevent errors

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -101,20 +101,44 @@ export function renderOrderSummary(){
     link.addEventListener('click',()=>{
       let newQuantity = Number(link.previousElementSibling.value);
       const { productId } = link.dataset;
-      const quantityLabel = document.querySelector(`.quantity-label-${productId}`);
-      const originalValue = Number(quantityLabel.textContent);
 
-      if(newQuantity <= 0){
-        newQuantity = originalValue;
+      try{
+        const productExists = cart.some(cartItem => cartItem.productId === productId);
+
+        if(!productExists){
+          throw new Error(`Attempted to update non-existent product ID: ${productId}. Exiting.`);
+        }
+
+        const cartItemContainer = link.closest('.cart-item-container');
+        const originalValue = Number(document.querySelector(`.quantity-label-${productId}`).value);
+
+        if(isNaN(newQuantity)){
+          link.previousElementSibling.value = originalValue;
+          link.previousElementSibling.setAttribute('type','number');
+          cartItemContainer.classList.remove('is-editing');
+          return;
+        }
+
+        const quantityLabel = document.querySelector(`.quantity-label-${productId}`);
+
+        if(newQuantity <= 0){
+          removeFromCart(productId);
+          cartItemContainer.remove();
+          renderHeader();
+          renderPaymentSummary();
+          return;
+        }
+
+        updateCartItemQuantity(productId, newQuantity);
+
+        quantityLabel.textContent = newQuantity;
+        link.closest('.cart-item-container').classList.remove('is-editing');
+
+        renderHeader();
+        renderPaymentSummary();
+      } catch (e){
+        console.warn(e);
       }
-
-      updateCartItemQuantity(productId, newQuantity);
-
-      quantityLabel.textContent = newQuantity;
-      link.closest('.cart-item-container').classList.remove('is-editing');
-
-      renderPaymentSummary();
-      renderHeader();
     });
   });
 
