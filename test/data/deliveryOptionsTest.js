23a654238e3b0318754636e81039c3f1d19a7cee
Jesus HM <jesus.h.mora0406@gmail.com>
2025-10-30 22:19:54 -0600

    test: add full suite for isWeekend function

--- test/data/deliveryOptionsTest.js
+++ test/data/deliveryOptionsTest.js
@@ -1,2 +1,44 @@
 import { deliveryOptions, isWeekend, calculateDeliveryDate } from "../../data/deliverOptions.js";
 import dayjs from "https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js";
+
+describe('isWeekend',()=>{
+  it('returns true if it is sunday',()=>{
+    // This day is sunday
+    let randomDate = new Date('October 20, 2002');
+    randomDate = dayjs(randomDate);
+
+    expect(isWeekend(randomDate)).toEqual(true);
+  });
+
+  it('returns true if it is saturday',()=>{
+    // This day is saturday
+    let randomDate = new Date('October 19, 2002');
+    randomDate = dayjs(randomDate);
+
+    expect(isWeekend(randomDate)).toEqual(true);
+  });
+
+  it('returns false if it is weekday',()=>{
+    // This day is monday
+    let randomDate = new Date('October 21, 2002');
+    randomDate = dayjs(randomDate);
+
+    expect(isWeekend(randomDate)).toEqual(false);
+  });
+
+  it('handles other dates',()=>{
+    // This day is sunday
+    let randomDate = new Date('March 2, 2025');
+    randomDate = dayjs(randomDate);
+
+    expect(isWeekend(randomDate)).toEqual(true);
+  });
+
+  it('handles other dates',()=>{
+    // This day is wednesday
+    let randomDate = new Date('December 25, 2019');
+    randomDate = dayjs(randomDate);
+
+    expect(isWeekend(randomDate)).toEqual(false);
+  });
+});
