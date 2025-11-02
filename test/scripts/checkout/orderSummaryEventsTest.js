556020670031813b064037b1c3582cfb866c93a1
Jesus HM <jesus.h.mora0406@gmail.com>
2025-11-01 19:51:28 -0600

    test: add full suite for handleDeleteLink

--- test/scripts/checkout/orderSummaryEventsTest.js
+++ test/scripts/checkout/orderSummaryEventsTest.js
@@ -22,3 +22,20 @@ describe('handleUpdateDeliveryOption',()=>{
     expect(updateDeliveryOptionSpy).toHaveBeenCalledWith('123', '3');
   });
 });
+
+describe('handleDeleteLink',()=>{
+  beforeEach(()=>{
+    spyOn(localStorage, 'setItem');
+  });
+
+  it('calls removeFunc with the correct values',()=>{
+    const mockOption = document.createElement('DIV');
+    mockOption.dataset.productId = '123';
+
+    const removeFuncSpy = jasmine.createSpy('removeFunc');
+    orderSummaryEventsModule.hanldeDeleteLink(mockOption, removeFuncSpy);
+
+    expect(removeFuncSpy).toHaveBeenCalledTimes(1);
+    expect(removeFuncSpy).toHaveBeenCalledWith('123');
+  });
+});
