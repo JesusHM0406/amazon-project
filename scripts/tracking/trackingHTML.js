d5db9df2539c50dc4e89984d7e175f9280a7d97f
Jesus HM <jesus.h.mora0406@gmail.com>
2025-11-05 12:19:03 -0600

    feat: create the function that generates the HTML of tracking page

---
+++ scripts/tracking/trackingHTML.js
@@ -0,0 +1,52 @@
+import { orders } from "../../data/orders.js";
+import { products } from "../../data/products.js";
+
+export function genereateTrackingHTML(orderId, productId){
+  const orderMatched = orders.find(order => order.orderId === orderId);
+  
+  if(!orderMatched) return 'Sorry, the order doesn\'t exists, verify the URL';
+  
+  const matchedProduct = orderMatched.products.find(product => product.productId === productId);
+
+  const productExists = products.find(product => product.id === productId);
+
+  if(!matchedProduct || !productExists) return 'Sorry, the product doesn\'t exists in any order, verify the URL';
+
+  return `
+    <div class="order-tracking">
+      <a class="back-to-orders-link link-primary" href="orders.html">
+        View all orders
+      </a>
+
+      <div class="delivery-date">
+        ${matchedProduct.deliveryDate}
+      </div>
+
+      <div class="product-info">
+        ${productExists.name}
+      </div>
+
+      <div class="product-info">
+        ${matchedProduct.quantity}
+      </div>
+
+      <img class="product-image" src="${productExists.image}">
+
+      <div class="progress-labels-container">
+        <div class="progress-label">
+          Preparing
+        </div>
+        <div class="progress-label current-status">
+          Shipped
+        </div>
+        <div class="progress-label">
+          Delivered
+        </div>
+      </div>
+
+      <div class="progress-bar-container">
+        <div class="progress-bar"></div>
+      </div>
+    </div>
+  `;
+};
